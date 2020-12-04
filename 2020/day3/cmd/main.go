6ab910c17fb10b57b3cce550eaa1c9feea4215c1
David Braley <davidbraley@Davids-MacBook-Pro.local>
2020-12-04 01:21:09 -0500

    Day 3, Part 2

--- 2020/day3/cmd/main.go
+++ 2020/day3/cmd/main.go
@@ -15,6 +15,20 @@ func main() {
 		os.Exit(1)
 	}
 
-	treeCount := day3.CountTreesOnPath(in)
-	fmt.Printf("Part 1: %v\n", treeCount)
+	treeCount3_1 := day3.CountTreesOnPath(in, 3, 1)
+	fmt.Printf("Part 1: %v\n", treeCount3_1)
+
+	treeCount1_1 := day3.CountTreesOnPath(in, 1, 1)
+	fmt.Printf("1-1 trees: %v\n", treeCount1_1)
+
+	treeCount5_1 := day3.CountTreesOnPath(in, 5, 1)
+	fmt.Printf("5-1 trees: %v\n", treeCount5_1)
+
+	treeCount7_1 := day3.CountTreesOnPath(in, 7, 1)
+	fmt.Printf("7-1 trees: %v\n", treeCount7_1)
+
+	treeCount1_2 := day3.CountTreesOnPath(in, 1, 2)
+	fmt.Printf("1-2 trees: %v\n", treeCount1_2)
+
+	fmt.Printf("Part 2: %v\n", treeCount1_1*treeCount3_1*treeCount5_1*treeCount7_1*treeCount1_2)
 }
--- 2020/day3/path.go
+++ 2020/day3/path.go
@@ -3,13 +3,17 @@ package day3
 const tree = '#'
 
 // CountTreesOnPath counts the trees on the 3 over, 1 down path, wrapping around the edges.
-func CountTreesOnPath(in []string) int {
+func CountTreesOnPath(in []string, right, down int) int {
 	treeCount := 0
-	for i, row := range in {
-		offset := (i * 3) % len(row)
-		// fmt.Println(i, offset)
-		c := []rune(row)[offset]
-		// fmt.Println(i, string(c))
+	y, x := 0, 0
+	for {
+		y += down
+		if y >= len(in) {
+			break
+		}
+		row := in[y]
+		x = (x + right) % len(row)
+		c := []rune(row)[x]
 		if c == tree {
 			treeCount = treeCount + 1
 		}
