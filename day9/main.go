c39b0fb1bc692ad4c3dd504feb2910e3e7cb499b
David Braley <davidbraley@khanacademy.org>
2022-12-09 22:32:15 -0500

    Day 9

---
+++ day9/main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"fmt"
+	"github.com/dbraley/advent-of-code/file"
+	"os"
+	"strconv"
+)
+
+type point struct {
+	X int
+	Y int
+}
+
+func (p point) move(xVec int, yVec int) point {
+	return point{X: p.X + xVec, Y: p.Y + yVec}
+}
+
+func (p point) follow(head point) (point, bool) {
+	needToMove := Abs(p.X-head.X) > 1 || Abs(p.Y-head.Y) > 1
+	if !needToMove {
+		return p, false
+	}
+	xVec, yVec := 0, 0
+	if head.X > p.X {
+		xVec = 1
+	} else if head.X < p.X {
+		xVec = -1
+	}
+	if head.Y > p.Y {
+		yVec = 1
+	} else if head.Y < p.Y {
+		yVec = -1
+	}
+	return p.move(xVec, yVec), true
+}
+
+func Abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func main() {
+	//f := "example.txt"
+	f := "input.txt"
+
+	//length := 2
+	length := 10
+
+	input, err := file.ReadSSV("day9/" + f)
+	if err != nil {
+		fmt.Printf("Error reading file %v\n", err)
+		os.Exit(1)
+	}
+
+	//fmt.Printf("%d\n", len(input))
+
+	tailVisits := make(map[point]int)
+
+	knots := make(map[int]point)
+
+	for i := 0; i < length; i++ {
+		knots[i] = point{X: 0, Y: 0}
+	}
+	tailVisits[knots[length-1]] = 1
+
+	for r, line := range input {
+		direction := line[0]
+		magnitude, _ := strconv.Atoi(line[1])
+		fmt.Printf("%v: %v %d\n", r, direction, magnitude)
+
+		xVec, yVec := 0, 0
+		switch direction {
+		case "R":
+			xVec = 1
+		case "L":
+			xVec = -1
+		case "U":
+			yVec = 1
+		case "D":
+			yVec = -1
+		default:
+			fmt.Printf("Unable to move in direction %v", direction)
+			os.Exit(2)
+		}
+
+		for m := 0; m < magnitude; m++ {
+			knots[0] = knots[0].move(xVec, yVec)
+			for k := 1; k < length; k++ {
+				moved := false
+				knots[k], moved = knots[k].follow(knots[k-1])
+				if moved && k == length-1 {
+					tailVisits[knots[length-1]]++
+				}
+			}
+
+			//fmt.Printf("Knots: %v\tTailVisits: %d\n", knots, len(tailVisits))
+		}
+
+	}
+
+	fmt.Printf("\n\nTail Visited Points: %d\n", len(tailVisits))
+}
